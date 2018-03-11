e51a805037f3425ee86d0f24f50c369840418b1a
Alexander Dodonov <pr0fedt@gmail.com>
2018-03-11 17:47:40 +0300

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 CWD=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 NVIM_TOOLS=~/.nvim-tools
+
+[ ! -d "~/.config/nvim" ] && mkdir ~/.config/nvim
+[ ! -d "$NVIM_TOOLS" ] && mkdir $NVIM_TOOLS
+[ -f "~/.config/nvim/init.vim" ] && rm -f /.config/nvim/init.vim
+
 # TODO remove comments below
 # OSX
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Linux
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+# curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 #    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Linux
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+pip install neovim
 cp $CWD/../config/init.vim ~/.config/nvim/init.vim
-mkdir $NVIM_TOOLS
 cp -R $CWD/../doc $NVIM_TOOLS/doc
 cp -R $CWD/../scripts $NVIM_TOOLS/scripts
 cp -R $CWD/../default $NVIM_TOOLS/default
